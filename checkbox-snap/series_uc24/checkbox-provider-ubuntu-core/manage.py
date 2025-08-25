6355cec7316482368d0b54250ca18cde6e685d70
Philip Meulengracht <the_meulengracht@hotmail.com>
2025-08-25 14:00:22 +0200

    add stuff

---
+++ checkbox-snap/series_uc24/checkbox-provider-ubuntu-core/manage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Copyright 2025 Canonical Ltd.
+# All rights reserved.
+
+"""Management script for the ubuntu-core provider."""
+
+from plainbox.provider_manager import setup
+from plainbox.provider_manager import N_
+
+setup(
+    name='checkbox-provider-ubuntu-core',
+    namespace='com.canonical.qa.ubuntu-core',
+    version="0.1",
+    description=N_("Checkbox Provider for ubuntu-core devices"),
+    gettext_domain='checkbox-provider-ubuntu-core',
+)
